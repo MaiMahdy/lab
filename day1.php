7641f0f9d36e3e063f2bd2fe5d29e3edd769a978
Mai mahdy <mai22yousef22@gmail.com>
2023-12-11 14:19:38 +0200

    day2

---
+++ day1.php
@@ -0,0 +1,68 @@
+
+<?php
+
+echo "Wellcome To php";
+echo "<br>";
+print "Wellcome To php";
+/Number 2/
+ $x = 5;
+ $y ="Welcome";
+ $z = true;
+ echo $x;
+ echo "<br>";
+echo $y;
+echo "<br>";
+ echo $z;
+ echo "<br>";
+
+ echo "x is : " . gettype($x);
+ echo "<br>";
+ echo "y is : " . gettype($y);
+echo "<br>";
+echo "z is : " . gettype($z);
+/Number 4/
+ $i = 0;
+ for( $i = 0; $i <= 15; $i++ ){
+    echo $i;
+    echo "<br>";
+}
+ while($i <= 15 ){
+    echo $i;
+   echo "<br>";
+    $i ++ ;
+}
+
+// const school= "ITI";
+// echo school;
+// echo "<br>";
+// define("CAR","BMW");
+// echo CAR;
+// $m = 5;
+// $n = 9;
+// $result = $m + $n;
+// if( $result > 50){
+//     echo "Accepted";
+// }else{
+//     echo "Not Accepted";
+// }
+
+// echo "<table border='1'>
+// <tr>
+//     <td>Salary Of Ahmed</td>
+//     <td>15000$</td>
+// </tr>
+// <tr>
+//     <td>Salary Of Salah</td>
+//     <td>15200$</td>
+// </tr>
+// <tr>
+//     <td>Salary Of Hosney</td>
+//     <td>14000$</td>
+// </tr>
+//     </table>";
+// $x = 651;
+// function anyToString($x){
+//        echo $x;
+// }
+// echo anyToString($x);
+?>
